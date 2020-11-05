6cfa148a4da7df43a0a1645080be059fc684ee7a
piotrmikula <p.mikula14@wp.pl>
2020-11-05 13:20:47 +0000

    Zadanie4

--- lab2.cpp
+++ lab2.cpp
@@ -15,9 +15,20 @@ public:
 
     double* vector;
 
-    void dlugosc_wek() { std::cout << "Dlugosc wektora wynosi " << dlugosc << std::endl; }
+    void dlugosc_wek() { std::cout << "Dlugosc wektora wynosi " << dlugosc << "\n" << std::endl; }
 
     int getDlugosc() { return dlugosc; }
+
+    void print()
+    {
+        std::cout << "Obecna zawartosc wektora [";
+        for (int i = 0; i < dlugosc; ++i) {
+            std::cout << vector[i];
+            if (i < (dlugosc - 1))
+                std::cout << ", ";
+        }
+        std::cout << "]\n" << std::endl;
+    }
 
 private:
     int dlugosc;
@@ -27,6 +38,7 @@ int main()
 {
     Wektor wek{4};
     wek.dlugosc_wek();
+    wek.print();
 
     std::cout << "Ostatnia linijka kodu." << std::endl;
 }
