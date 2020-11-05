20d866f3640fa9aced87fb3520838d3d8bda2efc
piotrmikula <p.mikula14@wp.pl>
2020-11-05 12:04:56 +0000

    Zadanie1

--- lab2.cpp
+++ lab2.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+class Wektor
+{
+public:
+    Wektor(int n_wek) { n = n_wek; }
+
+    ~Wektor() { delete[] vector; }
+
+    int     n;
+    double* vector = new double[n];
+
+    void dlugosc() { std::cout << "Dlugosc wektora wynosi " << n << std::endl; }
+};
+
 int main()
 {
-    puts("Hello, World!");
+    Wektor wek{4};
+    wek.dlugosc();
+
+    std::cout << "Ostatnia linijka kodu." << std::endl;
 }
