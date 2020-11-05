736a82ddd086ac9764de5c8f8b066cc10b214f44
piotrmikula <p.mikula14@wp.pl>
2020-11-05 12:18:52 +0000

    Zadanie2

--- lab2.cpp
+++ lab2.cpp
@@ -3,7 +3,12 @@
 class Wektor
 {
 public:
-    Wektor(int n_wek) { n = n_wek; }
+    Wektor(int n_wek)
+    {
+        n = n_wek;
+        for (int i = 0; i < n; i++)
+            vector[i] = 0;
+    }
 
     ~Wektor() { delete[] vector; }
 
@@ -11,12 +16,20 @@ public:
     double* vector = new double[n];
 
     void dlugosc() { std::cout << "Dlugosc wektora wynosi " << n << std::endl; }
+    void wartosci()
+    {
+        std::cout << "[ ";
+        for (int i = 0; i < n; i++)
+            std::cout << vector[i] << " ";
+        std::cout << "]" << std::endl;
+    }
 };
 
 int main()
 {
     Wektor wek{4};
     wek.dlugosc();
+    wek.wartosci();
 
     std::cout << "Ostatnia linijka kodu." << std::endl;
 }
