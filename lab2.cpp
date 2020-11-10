3ed699e0b3c0c8cce02269f1bf9d5a119649a409
piotrmikula <p.mikula14@wp.pl>
2020-11-10 09:43:23 +0000

    Zadanie14

--- lab2.cpp
+++ lab2.cpp
@@ -1,75 +1,22 @@
 #include <iostream>
 
-class Wektor
+struct Liczba
 {
-public:
-    Wektor(int n_wek) : dlugosc(n_wek), pojemnosc(dlugosc)
+    Liczba(int w) : wartosc{w} {}
+    Liczba(const Liczba& l) : wartosc{l.wartosc}
     {
-        vector = new double[dlugosc];
-        for (int i = 0; i < dlugosc; i++)
-            vector[i] = 0;
+        std::cout << "Utworzono konstruktor kopiujacy\n";
     }
 
-    ~Wektor() { delete[] vector; }
-
-    void dlugosc_wek() { std::cout << "Dlugosc wektora wynosi " << dlugosc << std::endl; }
-
-    int getDlugosc() { return dlugosc; }
-    int getPojemnosc() { return pojemnosc; }
-
-    void print()
-    {
-        std::cout << "Obecna zawartosc wektora [";
-        for (int i = 0; i < dlugosc; ++i) {
-            std::cout << vector[i];
-            if (i < (dlugosc - 1))
-                std::cout << ", ";
-        }
-        std::cout << "]\n" << std::endl;
-    }
-
-    void ZmienDlugosc(int nowe_n)
-    {
-        if (nowe_n <= pojemnosc) {
-            for (int i = nowe_n; i < pojemnosc; i++)
-                vector[i] = 0;
-            dlugosc = nowe_n;
-        }
-        else if (nowe_n > pojemnosc) {
-            double* temp = new double[nowe_n];
-            for (int i = 0; i < dlugosc; i++)
-                temp[i] = vector[i];
-            for (int i = dlugosc; i < nowe_n; i++)
-                temp[i] = 0.0;
-
-            vector = temp;
-            delete[] temp;
-
-            dlugosc   = nowe_n;
-            pojemnosc = dlugosc;
-        }
-    }
-
-    double& operator[](int n)
-    {
-        if (n > dlugosc) {
-            ZmienDlugosc(n);
-            return vector[n];
-        }
-        else
-            return vector[n];
-    }
-
-private:
-    double* vector;
-    int     dlugosc;
-    int     pojemnosc;
+    int wartosc;
 };
 
 int main()
 {
-    Wektor wek{4};
-    wek.dlugosc_wek();
-
-    std::cout << "Ostatnia linijka kodu." << std::endl;
+    std::cout << "Krok 1" << std::endl;
+    Liczba a{1};
+    std::cout << "Krok 2" << std::endl;
+    Liczba b{a};
+    std::cout << "Krok 3" << std::endl;
+    Liczba c = a;
 }
